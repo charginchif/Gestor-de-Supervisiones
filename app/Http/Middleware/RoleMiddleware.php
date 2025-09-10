30e203cb6689d62ed79e6834f7923847fdcdf3e9
charginchif <andredarksoul40@gmail.com>
2025-09-10 12:34:17 -0600

    error_log prints deleted

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -11,34 +11,27 @@ class RoleMiddleware
     public function handle($request, Closure $next, $rolRequerido)
     {
         $claims = $request->attributes->get('jwt_claims');
-       
 
         if (!$claims || !isset($claims['sub'])) {
-            error_log('RoleMiddleware: No claims or sub not set.');
             return RespuestaAPI::error('No autorizado para acceder a este recurso', RespuestaAPI::HTTP_NO_AUTORIZADO);
         }
 
         $usuario = User::find($claims['sub']);
 
         if (!$usuario) {
-            error_log('RoleMiddleware: User not found in DB.');
             return RespuestaAPI::error('Usuario no encontrado', RespuestaAPI::HTTP_NO_AUTORIZADO);
         }
 
         $rol = CatRol::where('nombre', $rolRequerido)->first();
 
         if (!$rol) {
-            error_log('RoleMiddleware: Role not found in DB: ' . $rolRequerido);
             return RespuestaAPI::error('Rol no válido', RespuestaAPI::HTTP_PROHIBIDO);
         }
 
         $requiredRoleId = $rol->id;
 
-        error_log('User ID: ' . $usuario->id . ', User Role ID: ' . $usuario->id_rol . ', Required Role ID: ' . ($requiredRoleId ?? 'null'));
-
         // Check if the user's role ID matches the required role ID
         if ($usuario->id_rol != $requiredRoleId) {
-            error_log('RoleMiddleware: Role check failed. User ID: ' . $usuario->id . ', User Role ID: ' . $usuario->id_rol . ', Required Role ID: ' . ($requiredRoleId ?? 'null'));
             return RespuestaAPI::error('No tienes permisos para acceder a este recurso', RespuestaAPI::HTTP_PROHIBIDO);
         }
 
